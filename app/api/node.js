601409061294d7dce604f8869b0a29b8b4603bbd
Douglas Eleutério <douglaseleuterio@gmail.com>
2022-07-01 19:15:17 -0300

    Finishing the mongo connect to store data

--- app/api/node.js
+++ app/api/node.js
@@ -1,14 +1,37 @@
-const http = require('http')
+const { MongoClient } = require('mongodb');
+const http = require('http');
 const port = 3000
 
+
+// Connection URL
+const url = 'mongodb://root:example@mongo:27017/';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'people';
+const colName = 'people';
+let db = null;
+
 const requestHandler = (req, res) => {
+    const collection = db.collection(colName)
     if (req.url === '/api/people' && req.method === 'GET') {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.statusCode = 200;
-        res.end(JSON.stringify(['Welcome Home', 'Doug', 'Annie']));
-    } else if (req.url === '/people' && req.method === 'POST') {
-        // res.write('Doug');
-        res.end('ok');
+        collection.find({}).toArray().then(data => {
+            res.statusCode = 200;
+            res.end(JSON.stringify(data));
+        });
+
+    } else if (req.url === '/api/people' && req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            const data = JSON.parse(body);
+            collection.insertMany([{ name: data.name }]).then(data => {
+                res.statusCode = 200;
+                res.end(data.name);
+            });
+        });
     } else {
         res.statusCode = 404;
         res.end('404: Path not found');
@@ -17,10 +40,14 @@ const requestHandler = (req, res) => {
 
 const server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
+client.connect().then(() => {
+    db = client.db(dbName)
+    server.listen(port, (err) => {
+        if (err) {
+          return console.log('something bad happened', err)
+        }
+      
+        console.log(`server is listening on ${port}`)
+      })
+      
 })
